de0425ebecfa207ecb0500cd004e53dabf38a28d
* <therror@my.com>
2020-12-02 17:16:54 +0300

    Fix label and setup.py...

--- setup.py
+++ setup.py
@@ -1,23 +1,5 @@
 import sys
-import json
-
 from setuptools import setup, find_packages
-from setuptools.command.test import test
-
-
-class PyTest(test):
-    def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(['tests']))
-
-
-def requirements(section):
-    """Создание листа зависимостей для этого проекта."""
-    with open('Pipfile.lock') as pip_file:
-        pipfile_json = json.load(pip_file)
-
-    return [package + detail.get('version', '')
-            for package, detail in pipfile_json[section].items()]
 
 
 packages = find_packages()
@@ -29,17 +11,17 @@ with open('CHANGES.rst', 'r', encoding='utf-8') as f:
     changes = f.read()
 
 
-setup(name='yandex-music',
+setup(name='async-yandex-music',
       version='0.1.2',
-      author='Il`ya Semyonov',
+      author='Il`ya Semyonov : therrors',
       author_email='Ilya@marshal.by',
       license='LGPLv3',
-      url='https://github.com/MarshalX/yandex-music-api/',
-      keywords='python yandex music api wrapper library питон пайтон яндекс музыка апи обёртка библиотека',
+      url='https://github.com/therrors/async-yandex-music-api/',
+      keywords='python yandex music api wrapper library async питон пайтон яндекс музыка апи обёртка библиотека',
       description='Делаю то, что по определённым причинам не сделала компания Yandex.',
       long_description=f'{readme}\n{changes}',
       packages=packages,
-      install_requires=requirements('default'),
+      install_requires=['aiohttp'],
       include_package_data=True,
       classifiers=[
           'Development Status :: 3 - Alpha',
@@ -55,8 +37,6 @@ setup(name='yandex-music',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8'
       ],
-      cmdclass={'test': PyTest},
-      tests_require=requirements('develop'),
       project_urls={
           'Code': 'https://github.com/MarshalX/yandex-music-api',
           'Documentation': 'https://yandex-music.readthedocs.io',
